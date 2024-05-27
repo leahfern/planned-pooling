61f58c460a29a9d84158602c10ce4d118314abfa
Leah Tabush <leahtabush@gmail.com>
2024-05-27 15:27:59 -0700

    move getRandomRGBColor to utils file

--- src/colorEditor/AddColor.jsx
+++ src/colorEditor/AddColor.jsx
@@ -1,17 +1,7 @@
 import React from 'react';
+import { getRandomRGBColor } from '../utils/colorUtils.js';
 
 const AddColor = ({ colorSequence, setColorSequence }) => {
-  const getRandomRGBColor = () => {
-    // Generate random values for red, green, and blue components
-    const red = Math.floor(Math.random() * 256); // Random integer between 0 and 255
-    const green = Math.floor(Math.random() * 256);
-    const blue = Math.floor(Math.random() * 256);
-
-    // Construct the RGB color string
-    const rgbColor = `rgb(${red}, ${green}, ${blue})`;
-    return rgbColor;
-  };
-
   const numStitches = Math.floor(Math.random() * 5) + 1;
 
   const newColorItem = {
--- src/utils/colorUtils.js
+++ src/utils/colorUtils.js
@@ -28,3 +28,14 @@ export const hexToRgb = (hex) => {
       )})`
     : null;
 };
+
+export const getRandomRGBColor = () => {
+  // Generate random values for red, green, and blue components
+  const red = Math.floor(Math.random() * 256); // Random integer between 0 and 255
+  const green = Math.floor(Math.random() * 256);
+  const blue = Math.floor(Math.random() * 256);
+
+  // Construct the RGB color string
+  const rgbColor = `rgb(${red}, ${green}, ${blue})`;
+  return rgbColor;
+};
